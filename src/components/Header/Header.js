e28ec97cfc4012178d60bcc50baa584ecbd84510
Qusay114 <eng.qusay015@gmail.com>
2021-05-23 11:11:56 +0300

    fix the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -85,7 +85,7 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
               {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
               <MDBCollapse isOpen={this.state.collapse} navbar>
                 <MDBNavbarNav left>
-                  <MDBNavItem active>
+                  <MDBNavItem >
                     <Nav.Link href="http://localhost:3000">Home</Nav.Link>
                   </MDBNavItem>
                   <MDBNavItem>
@@ -114,10 +114,9 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
             <MDBMask overlay="purple-light" className="flex-center flex-column text-white text-center">
               <div id='test'>
                 <div id="text">
-                  <h2>This Navbar is fixed</h2>
-                  <h5>It will always stay visible on the top, even when you scroll down</h5>
-                  <p>Navbar's background will switch from transparent to solid color while scrolling down</p><br />
-                  <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+                  <h2>Lorem Text</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    <h3></h3> Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
               </div>
             </MDBMask>
