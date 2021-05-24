0d58abe58962b7b8a81b45aac607d98c8cb673f6
Qusay114 <eng.qusay015@gmail.com>
2021-05-25 00:22:36 +0300

    change the link tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBContainer, MDBMask, MDBView } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Nav , Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 //  auth library
 import { withAuth0 } from '@auth0/auth0-react';
@@ -95,13 +96,15 @@ class FullPageIntroWithFixedTransparentNavbar extends React.Component {
                     {isAuthenticated &&
 
                     <>
-                      <Nav.Link href={`${process.env.REACT_APP__HOME_URL}/profile`} >Profile</Nav.Link>
+                      {/* <Nav.Link href={`${process.env.REACT_APP__HOME_URL}/profile`} >Profile</Nav.Link> */}
+                      <Link href={`${process.env.REACT_APP__HOME_URL}/profile`} >Profile</Link>
                     </>
 
                     }
                   </MDBNavItem>
                   <MDBNavItem>
-                    <Nav.Link href={`${process.env.REACT_APP__HOME_URL}/aboutus`}>About Us</Nav.Link>
+                    {/* <Nav.Link href={`${process.env.REACT_APP__HOME_URL}/aboutus`}>About Us</Nav.Link> */}
+                    <Link href={`${process.env.REACT_APP__HOME_URL}/aboutus`}>About Us</Link>
                   </MDBNavItem>
                   <MDBNavItem>
                     {!isAuthenticated &&
